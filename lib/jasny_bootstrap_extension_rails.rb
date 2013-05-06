d586dd674e87ac9900766c9798c98d6dbab24798
Matthew de Detrich <mdedetrich@gmail.com>
2013-05-06 17:07:49 +1000

    - Added in the lib

--- lib/jasny_bootstrap_extension_rails.rb
+++ lib/jasny_bootstrap_extension_rails.rb
@@ -1,2 +1,9 @@
 module JasnyBootstrapExtensionRails
+  module Rails
+    if defined?(::Rails) and Gem::Requirement.new('>= 3.2').satisfied_by?(Gem::Version.new ::Rails.version)
+      class Rails::Engine < ::Rails::Engine
+        # this class enables the asset pipeline
+      end
+    end
+  end
 end
